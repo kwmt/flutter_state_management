b652e8548cac52ed46e6b72caa6ee41cef461a26
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-05-18 10:54:47 +0900

    providerによりStatelessWidgetでよくなったので変更

--- lib/slider.dart
+++ lib/slider.dart
@@ -2,14 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:slider_state_management/MySchedule.dart';
 
-class MySlider extends StatefulWidget {
-  @override
-  _MySliderState createState() => _MySliderState();
-}
-
-class _MySliderState extends State<MySlider> {
-  var _value = 0.5;
-
+class MySlider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final schedule = Provider.of<MySchedule>(context);
@@ -17,9 +10,5 @@ class _MySliderState extends State<MySlider> {
       value: schedule.stateManagementTime,
       onChanged: (value) => schedule.stateManagementTime = value,
     );
-  }
-
-  void _onValueChanged(double value) {
-    setState(() => _value = value);
   }
 }
